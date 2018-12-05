da6b9f10e4952b2faf7ae2e6faa59f3168b652e4
unknown <saswata@moviloglobal.com>
2018-12-06 01:44:07 +0530

    4:21 Custom error pages.

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,11 +5,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * @Route("/blog")
