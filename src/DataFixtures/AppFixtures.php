1347316ebb1270082f5334b37fa2b86fe24df0f2
unknown <saswata@moviloglobal.com>
2018-12-06 19:32:29 +0530

    6:38 User Entity Fixtures.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,12 +3,30 @@
 namespace App\DataFixtures;
 use App\Entity\MicroPost;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
+    {
+        $this->loadMicroPosts($manager);
+        $this->loadUsers($manager);
+    }
+
+    private function loadMicroPosts(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
@@ -19,6 +37,18 @@ class AppFixtures extends Fixture
         	$manager->persist($microPost);
         }
 
+        $manager->flush();
+    }
+
+    private function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('saswatab');
+        $user->setFullname('Saswata Banerjee');
+        $user->setEmail('saswata@moviloglobal.com');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'saswata123'));
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
