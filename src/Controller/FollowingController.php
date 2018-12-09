6fa89ade6fd42b797da3520b2459d856953ee6de
unknown <saswata@moviloglobal.com>
2018-12-09 23:59:43 +0530

    7:58 Follow/Unfollow functionality (adding Entities to Collections).

---
+++ src/Controller/FollowingController.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Controller;
+use App\Entity\User;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Security("is_granted('ROLE_USER')")
+ * @Route("/following")
+ */
+Class FollowingController extends Controller
+{
+	/**
+	 * @Route("/follow/{id}", name="following_follow")
+	 */
+	public function follow(User $userToFollow)
+	{
+		/** @var User $currentUser */
+		$currentUser = $this->getUser();
+		$currentUser->getFollowing()->add($userToFollow);
+
+		$this->getDoctrine()->getManager()->flush();
+
+		return $this->redirectToRoute('micro_post_user', ['username' => $userToFollow->getUsername()]);
+	}
+
+	/**
+	 * @Route("/unfollow/{id}", name="following_unfollow")
+	 */
+	public function unfollow(User $userToUnfollow)
+	{
+		/** @var User $currentUser */
+		$currentUser = $this->getUser();
+		$currentUser->getFollowing()->removeElement($userToUnfollow);
+
+		$this->getDoctrine()->getManager()->flush();
+
+		return $this->redirectToRoute('micro_post_user', ['username' => $userToUnfollow->getUsername()]);		
+	}
+}
