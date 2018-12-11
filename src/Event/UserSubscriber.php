7fbb2ab777471024f57f0753eb3944ff2821ae8e
unknown <saswata@moviloglobal.com>
2018-12-11 16:35:13 +0530

    9:79 Sending email after user registers using Swift Mailer.

--- src/Event/UserSubscriber.php
+++ src/Event/UserSubscriber.php
@@ -5,6 +5,21 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class UserSubscriber implements EventSubscriberInterface
 {
+	/**
+	 * @var \Swift_Mailer
+	 */
+	private $mailer;
+	/**
+	 * @var \Twig_Environment
+	 */
+	private $twig;
+
+	public function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig)
+	{
+		$this->mailer = $mailer;
+		$this->twig = $twig;
+	}
+
 	public static function getSubscribedEvents()
 	{
 		return [UserRegisterEvent::NAME => 'onUserRegister'];
@@ -12,6 +27,14 @@ class UserSubscriber implements EventSubscriberInterface
 
 	public function onUserRegister(UserRegisterEvent $event)
 	{
-		
+		$body = $this->twig->render('email/registration.html.twig', ['user' => $event->getRegisteredUser()]);
+
+		$message = (new \Swift_Message())
+			->setSubject('Welcome to the Micro-Post App')
+			->setFrom('user@micropost.com')
+			->setTo($event->getRegisteredUser()->getEmail())
+			->setBody($body, 'text/html');
+
+		$this->mailer->send($message);
 	}
 }
