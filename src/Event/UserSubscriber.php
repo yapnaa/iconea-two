5eab8110f006d9f784547d38e9ace0571721d14e
unknown <saswata@moviloglobal.com>
2018-12-11 17:15:22 +0530

    9:81 Creating a mailer class to handle email sending (and generation using Twig).

--- src/Event/UserSubscriber.php
+++ src/Event/UserSubscriber.php
@@ -6,18 +6,13 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class UserSubscriber implements EventSubscriberInterface
 {
 	/**
-	 * @var \Swift_Mailer
+	 * @var Mailer
 	 */
 	private $mailer;
-	/**
-	 * @var \Twig_Environment
-	 */
-	private $twig;
 
-	public function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig)
+	public function __construct(Mailer $mailer)
 	{
 		$this->mailer = $mailer;
-		$this->twig = $twig;
 	}
 
 	public static function getSubscribedEvents()
@@ -27,14 +22,6 @@ class UserSubscriber implements EventSubscriberInterface
 
 	public function onUserRegister(UserRegisterEvent $event)
 	{
-		$body = $this->twig->render('email/registration.html.twig', ['user' => $event->getRegisteredUser()]);
-
-		$message = (new \Swift_Message())
-			->setSubject('Welcome to the Micro-Post App')
-			->setFrom('user@micropost.com')
-			->setTo($event->getRegisteredUser()->getEmail())
-			->setBody($body, 'text/html');
-
-		$this->mailer->send($message);
+		$this->mailer->sendConfirmationEmail($event->getRegisteredUser());
 	}
 }
