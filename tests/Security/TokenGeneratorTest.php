3b77fd3de5c170dfac74a7c7881b2441bff96b4b
unknown <saswata@moviloglobal.com>
2018-12-13 03:00:02 +0530

    12:94 Writing first unit test.

---
+++ tests/Security/TokenGeneratorTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Tests\Security;
+
+use App\Security\TokenGenerator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * 
+ */
+class ClassName extends TestCase
+{
+	public function testTokenGeneration()
+	{
+		$tokenGen = new TokenGenerator();
+		$token = $tokenGen->getRandomSecureToken(30);
+		#$token[15] = "*";
+
+		$this->assertEquals(30, strlen($token));
+		$this->assertEquals(1, preg_match("/[A-Za-z0-9]/", $token));
+		$this->assertTrue(ctype_alnum($token), 'Token contains incorrect characters');
+	}
+}
